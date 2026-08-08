2ee7979a360d4ef18f7731e20ae86e7056f90e10
scali790 <36126576+scali790@users.noreply.github.com>
2026-08-08 08:27:28 +0200

    route qualified SEO expansion pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,6 +16,11 @@ import {
   MedicalStampsPage, DigitalStampGuidePage, PdfGuidePage, FormatsGuidePage,
   CompanyRequirementsGuidePage, FAQPage,
 } from "./pages/SeoContent";
+import {
+  StatusStampsPage, ApprovedStampPage, ReceivedStampPage, PaidStampPage,
+  ConfidentialStampPage, DigitalVsRubberGuidePage, ShapeGuidePage,
+  TransparentPngGuidePage,
+} from "./pages/SeoExpansion";
 
 function Router() {
   return (
@@ -29,10 +34,18 @@ function Router() {
       <Route path="/templates/business-stamps" component={BusinessStampsPage} />
       <Route path="/templates/notary-stamps" component={NotaryStampsPage} />
       <Route path="/templates/medical-stamps" component={MedicalStampsPage} />
+      <Route path="/templates/status-stamps" component={StatusStampsPage} />
+      <Route path="/templates/approved-stamp" component={ApprovedStampPage} />
+      <Route path="/templates/received-stamp" component={ReceivedStampPage} />
+      <Route path="/templates/paid-stamp" component={PaidStampPage} />
+      <Route path="/templates/confidential-stamp" component={ConfidentialStampPage} />
       <Route path="/guides/what-is-a-digital-stamp" component={DigitalStampGuidePage} />
       <Route path="/guides/how-to-add-a-stamp-to-a-pdf" component={PdfGuidePage} />
       <Route path="/guides/png-vs-svg-vs-pdf-stamp" component={FormatsGuidePage} />
       <Route path="/guides/company-stamp-requirements" component={CompanyRequirementsGuidePage} />
+      <Route path="/guides/digital-vs-rubber-stamp" component={DigitalVsRubberGuidePage} />
+      <Route path="/guides/round-vs-rectangular-stamp" component={ShapeGuidePage} />
+      <Route path="/guides/transparent-png-stamp" component={TransparentPngGuidePage} />
       <Route path="/faq" component={FAQPage} />
       <Route path="/download" component={DownloadPage} />
       <Route path="/account" component={Account} />
@@ -50,13 +63,9 @@ function App() {
   return (
     <ErrorBoundary>
       <ThemeProvider defaultTheme="light">
-        <TooltipProvider>
-          <Toaster />
-          <Router />
-        </TooltipProvider>
+        <TooltipProvider><Toaster /><Router /></TooltipProvider>
       </ThemeProvider>
     </ErrorBoundary>
   );
 }
-
 export default App;
